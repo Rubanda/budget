5625b9aae19952eb2fca57f41d7dfb70a3cfc067
@moise.muhire <moise.muhire@gunsel.com.tr>
2024-06-11 08:55:47 +0300

    login is working

---
+++ prisma/migrations/20240611054707_1/migration.sql
@@ -0,0 +1,85 @@
+-- CreateTable
+CREATE TABLE `Account` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `provider` VARCHAR(191) NOT NULL,
+    `providerAccountId` VARCHAR(191) NOT NULL,
+    `refresh_token` TEXT NULL,
+    `access_token` TEXT NULL,
+    `expires_at` INTEGER NULL,
+    `token_type` VARCHAR(191) NULL,
+    `scope` VARCHAR(191) NULL,
+    `id_token` TEXT NULL,
+    `session_state` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `Account_userId_idx`(`userId`),
+    UNIQUE INDEX `Account_provider_providerAccountId_key`(`provider`, `providerAccountId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Session` (
+    `id` VARCHAR(191) NOT NULL,
+    `sessionToken` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Session_sessionToken_key`(`sessionToken`),
+    INDEX `Session_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NULL,
+    `emailVerified` DATETIME(3) NULL,
+    `username` VARCHAR(191) NULL,
+    `image` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_username_key`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `BudgetItem` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `description` VARCHAR(191) NOT NULL,
+    `quantity` INTEGER NOT NULL,
+    `unitPrice` DOUBLE NOT NULL,
+    `total` DOUBLE NOT NULL,
+    `amountPaid` DOUBLE NOT NULL,
+    `remaining` DOUBLE NOT NULL,
+    `inCharge` VARCHAR(191) NULL,
+    `categoryId` INTEGER NULL,
+
+    INDEX `BudgetItem_categoryId_idx`(`categoryId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Category` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `sectionId` INTEGER NULL,
+
+    INDEX `Category_sectionId_idx`(`sectionId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Section` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ src/components/icons.tsx
@@ -0,0 +1,72 @@
+
+import { 
+    FaceIcon, 
+    ImageIcon, 
+    SunIcon,
+    HamburgerMenuIcon,
+    MixIcon,
+    DashboardIcon,
+    PersonIcon,
+    ChevronLeftIcon,
+    ReloadIcon,
+    CalendarIcon,
+    PlusIcon,
+    RocketIcon,
+    DownloadIcon,
+    CheckCircledIcon,
+    CircleBackslashIcon,
+    CrossCircledIcon,
+    EnvelopeClosedIcon,
+    RadiobuttonIcon,
+    ExternalLinkIcon,
+    TrashIcon,
+    Pencil2Icon,
+    Pencil1Icon,
+    ChatBubbleIcon,
+} from "@radix-ui/react-icons"
+
+
+export const Icons = {
+  face: FaceIcon,
+  image: ImageIcon,
+  sun: SunIcon,
+  menu: HamburgerMenuIcon,
+  mix: MixIcon,
+  dashboard: DashboardIcon,
+  user: PersonIcon,
+  chevronLeft: ChevronLeftIcon,
+  spinner: ReloadIcon,
+  calendar: CalendarIcon,
+  plus: PlusIcon,
+  rocket: RocketIcon,
+  download: DownloadIcon,
+  status: CheckCircledIcon,
+  circlelips: CircleBackslashIcon,
+  crossedcircle: CrossCircledIcon,
+  envelope: EnvelopeClosedIcon,
+  radio: RadiobuttonIcon,
+  externalLink:ExternalLinkIcon,
+  checkCircle: CheckCircledIcon,
+  check: CheckCircledIcon,
+  trash: TrashIcon,
+  pencil: Pencil1Icon,
+  chat: ChatBubbleIcon,
+  google: ({ ...props }: any) => (
+    <svg width="50px"
+      height="50px"
+      viewBox="0 0 48 48"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}>
+      <title>google</title>
+      <g id="Layer_2" data-name="Layer 2">
+        <g id="invisible_box" data-name="invisible box">
+          <rect width="48" height="48" fill="none" />
+          <rect width="48" height="48" fill="none" />
+        </g>
+        <g id="icons_Q2" data-name="icons Q2">
+          <path d="M24.7,20.5v7.6H35.6a10.9,10.9,0,0,1-10.9,8,12.1,12.1,0,1,1,7.9-21.3l5.6-5.6A20,20,0,1,0,24.7,44c16.8,0,20.5-15.7,18.9-23.5Z" />
+        </g>
+      </g>
+    </svg>
+  ),
+}
