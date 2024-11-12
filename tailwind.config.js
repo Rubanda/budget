00e647f882bce11f01ce7779a3e955e7c91c805b
Moise Muhire <rubanda57@gmail.com>
2024-11-13 01:21:45 +0200

    fixed issue in tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-import type { Config } from "tailwindcss"
 
 const config = {
   darkMode: ["class"],
@@ -93,6 +92,6 @@ const config = {
   	}
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} 
 
 export default config
