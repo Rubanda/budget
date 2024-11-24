b5a3b46920d7b57e7005404cfbe01ecd901ee421
Moise Muhire <rubanda57@gmail.com>
2024-11-24 02:58:04 +0200

    working on it

--- src/components/members/edit-member.tsx
+++ src/components/members/edit-member.tsx
@@ -26,12 +26,10 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { DashboardShell } from "../shell";
-import { groupEditSchema } from "@/lib/validations/group";
 import { toast } from "sonner";
 import { useMutation } from "@tanstack/react-query";
 import React from "react";
 import { DialogDescription } from "@radix-ui/react-dialog";
-import { useFetchData } from "../fetch";
 import { config } from "@/utils/config";
 import { useSession } from "next-auth/react";
 import { Textarea } from "../ui/textarea";
@@ -43,66 +41,19 @@ type EditGroupProps = {
   setOpen: (open: boolean) => void;
   refetch: () => void;
 };
+export const MemberSchema = z.object({
+  name: z.string(),
+});
 const EditGroup = ({ id, open, setOpen, refetch }: EditGroupProps) => {
   const { data: session } = useSession();
 
   const [isSaving, setIsSaving] = React.useState(false);
   // fetch group
-  const {
-    data: result,
-    isLoading,
-    error,
-  } = useFetchData(`/api/groups/${id}`, ["group"]);
-
-  const { data: department, isLoading: departmentLoading } = useFetchData(`/api/departments`, ["departments"])
-  const final = {
-    ...result,
-    departmentId: JSON.stringify(result?.departmentId!),
-  };
-  const form = useForm<z.infer<typeof groupEditSchema>>({
-    resolver: zodResolver(groupEditSchema),
-    values: final,
-    mode: "onChange",
+  const form = useForm<z.infer<any>>({
+    resolver: zodResolver(MemberSchema),
   });
-  async function onSubmit(data: z.infer<typeof groupEditSchema>) {
-    setIsSaving(true);
-    changeGroup(data);
-    setIsSaving(false);
-  }
 
-  const { mutate: changeGroup } = useMutation({
-    mutationFn: async (data: z.infer<typeof groupEditSchema>) => {
-      const payload = {
-        ...data,
-        departmentId: +data?.departmentId!,
-      };
-      const res = await fetch(`${config.backendUrl}/api/groups/${id}`, {
-        method: "PATCH",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${session?.user?.token!}`,
-        },
-        body: JSON.stringify(payload),
-      });
-      const newData = await res.json();
-      return newData;
-    },
-    onError: (err) => {
-      console.log("[err::Group>>>]", err);
-
-      toast.error("Error Editing.");
-    },
-    onSuccess: async (data) => {
-      toast.success("Group has been updated.");
-      refetch();
-      setOpen(false);
-
-      form.reset();
-    },
-    // reload the page
-  });
-  if (isLoading) return <div>Loading...</div>;
+ 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className=" mx-auto rounded-xl max-w-[90%]  md:max-w-[500px] overflow-y-auto ">
@@ -112,7 +63,7 @@ const EditGroup = ({ id, open, setOpen, refetch }: EditGroupProps) => {
         <DashboardShell>
           <Form {...form}>
             <form
-              onSubmit={form.handleSubmit(onSubmit)}
+              // onSubmit={form.handleSubmit(onSubmit)}
               className="flex flex-col gap-3 "
             >
               <div className="flex flex-col gap-3 border-none ">
@@ -133,53 +84,7 @@ const EditGroup = ({ id, open, setOpen, refetch }: EditGroupProps) => {
                 />
 
 
-                <FormField
-                  control={form.control}
-                  name="departmentId"
-                  render={({ field }) => (
-                    <FormItem className="flex flex-col">
-                      {/* <FormLabel className="text-medium text-muted-foreground font-light">Department</FormLabel> */}
-                      <Select
-                        onValueChange={field.onChange}
-                        defaultValue={field.value?.toString()}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue
-                              placeholder={field.value?.toString()}
-                            />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          {departmentLoading && (
-                            <SelectItem value="Loading...">
-                              {" "}
-                              Loading...{" "}
-                            </SelectItem>
-                          )}
-                          {
-                            // check if department is available and map through it 
-
-                            department &&
-                            (department?.departments?.map(
-                              (
-                                item: { name: string; id: string },
-                                index: number
-                              ) => (
-                                <SelectItem
-                                  key={index}
-                                  value={item?.id.toString()}
-                                >
-                                  {item?.name}
-                                </SelectItem>
-                              )
-                            ))}
-                        </SelectContent>
-                      </Select>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
+             
                 <FormField
                   control={form.control}
                   name="description"
